3dc6226893cca26befeeee1c52c284095f994ab7
Madster973 <50129506+Madster973@users.noreply.github.com>
2022-01-23 18:57:41 +0530

    Time: 527 ms (33.89%), Space: 22.5 MB (69.64%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -13,6 +13,10 @@ class Solution(object):
         return False
         Time Limit exceeded
         """
-        return len(nums) != len(set(nums))
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
         
         
