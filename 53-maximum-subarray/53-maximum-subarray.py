04e3801eac418349e9ba40d9caf11cb69b9a80d3
Madster973 <50129506+Madster973@users.noreply.github.com>
2022-01-23 23:24:08 +0530

    Time: 861 ms (32.07%), Space: 25.7 MB (24.84%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -3,37 +3,6 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
-        from sys import maxsize
-To Track indexes of start and end
-def maxSubArraySum(a, size):
-    max_so_far = -maxsize - 1
-    max_ending_here = 0
-    start = 0
-    end = 0
-    s = 0
-
-    for i in range(0, size):
-
-        max_ending_here += a[i]
-
-        if max_so_far < max_ending_here:
-            max_so_far = max_ending_here
-            start = s
-            end = i
-
-        if max_ending_here < 0:
-            # in case of negative numbers we are making max_ending_here = 0
-            # because if we encounter a positive integer we would be starting the train from there not from negative
-            print(max_ending_here)
-            max_ending_here = 0
-            s = i + 1
-            print(s)
-
-    print("Maximum contiguous sum is %d" % (max_so_far))
-    print("Starting Index %d" % (start))
-    print("Ending Index %d" % (end))
-a = [-1,-2,-3,4]
-maxSubArraySum(a,len(a))
         """
         cur_sum = nums[0]
         max_sum = nums[0]
