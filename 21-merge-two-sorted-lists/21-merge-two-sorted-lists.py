3a81031eedb798af511f5ab0787c9c03e2935cbf
Madster973 <50129506+Madster973@users.noreply.github.com>
2022-03-20 00:28:08 +0530

    Time: 60 ms (5.19%), Space: 13.5 MB (61.35%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -10,8 +10,13 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        # Maintain dummy node to get the structure of New from starting
+        # We won't be able to return the New node as we are incrementing it
         dummy = New = ListNode(0)
+        # Running till one of the list is exhausted
         while list1 and list2:
+            # Compare the list1 and list2 values and assign the next to the list which has less
+            # value. Increment the assigned list and the new list
             if list1.val <= list2.val:
                 New.next = list1
                 list1=list1.next
@@ -20,6 +25,8 @@ class Solution(object):
                 New.next = list2
                 list2=list2.next
                 New = New.next
+        # As one of the list is exhausted, you need to assign the next of new to the list
+        # which has elements left (none or element = element)
         New.next = list1 or list2
         return dummy.next
         
