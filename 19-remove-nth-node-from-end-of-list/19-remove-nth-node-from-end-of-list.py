4cc6ebac71a0c95e4ec4cda951977957db66f6f4
Madster973 <50129506+Madster973@users.noreply.github.com>
2022-03-20 12:27:20 +0530

    Time: 28 ms (52.41%), Space: 13.4 MB (42.89%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -10,11 +10,18 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
+        # Assigning two pointers slow and fast
         slow=fast=head
+        # Incrementing the fast pointer by the times the index of the node to be deleted
         for i in range(n):
             fast = fast.next
+        # After incrementing the fast node if it is none it means that there are only 
+        # 1 or 2 values in the array and we are deleting the first 1
+        # Example [1,2] 1 or [1] 1
         if not fast:
             return head.next
+        # Increment the fast and slow simultaneously, by the time the fast reaches end the slow
+        # would be at the pointer where we need to delete the node
         while fast and fast.next:
             slow = slow.next
             fast = fast.next
