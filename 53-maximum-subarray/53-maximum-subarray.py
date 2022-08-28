31d5487e7335b4430aa8a89a86b00c42668492a2
Madster973 <50129506+Madster973@users.noreply.github.com>
2022-08-28 19:28:59 +0530

    Update 53-maximum-subarray.py

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,13 +1,18 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        cur_sum = nums[0]
-        max_sum = nums[0]
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Sliding window approach
+        # total = 0
+        # max_total = nums[0]
+        # for i in nums:
+        #     if total<0:
+        #         total = 0
+        #     total+=i
+        #     max_total = max(total,max_total)
+        # return max_total
+        # DP Approach
+        max_total = nums[0]
         for i in range(1,len(nums)):
-            cur_sum = max(nums[i],nums[i]+cur_sum)
-            max_sum = max(cur_sum,max_sum)
-        return max_sum
-    
+            nums[i] = max(nums[i],nums[i]+nums[i-1])
+            max_total = max(max_total,nums[i])
+        return max_total
+            
